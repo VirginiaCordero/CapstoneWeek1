98d2e7edef403c7ff0ea097a9a7afa1ed500c97f
virginia <mavirginia.cordero@gmail.com>
2018-07-22 23:56:06 -0400

    fixed program

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -7,55 +7,64 @@ public class PigLatin {
 		Scanner scanner = new Scanner(System.in);
 		
 		String userInput;
+		System.out.println("Welcome to the Pig Latin Translator");
 		
 		//This loop will ask the user to enter a word to translate to pig Latin
-		do {
-			System.out.println("Welcome to the Pig Latin Translator");
+		do {			
 			System.out.println("Please enter the word to translate: ");
-			String wordToTranslate = scanner.next(); 
-			wordToTranslate = wordToTranslate.toLowerCase(); // Converting word to lower case! (build spec 1)
+			String wordToCheck = scanner.next(); 
 			
-		    // Call method to find out if firstLetter is a vowel, if so add "way" at the end
+			if (wordToCheck.length() == 0) {
+				System.out.println("You need a full word, try harder.");
+				break;
+			}
 			
-			String wordToCheck = hasVowel(wordToTranslate);
-		    System.out.println(wordToTranslate + "way");
-		     
-		    // Call method to find out if it is consonant, if so 
-		    // find position of vowel
-		    // print substring after vowel position
-		    // move consonants before first vowel to the end 
-		    // add "ay" to the end
-		     
-			String firstLetter = wordToTranslate.substring(0,1); //To remove vowel at the beginning, locate first letter.
-		    String restOfTheWord = wordToTranslate.substring(1); // To print the word without the first
-		    System.out.println(restOfTheWord + firstLetter + "way");
+			wordToCheck = wordToCheck.toLowerCase(); // Converting word to lower case! (build spec 1)
 			
-			//Asking the user if she/he wishes to continue
+			boolean isFirstLetterAVowel = isFirstLetterAVowel(wordToCheck);
+			
+			if (isFirstLetterAVowel == true) {
+				wordToCheck += "way";	
+				System.out.println(wordToCheck);
+			} else {
+				int foundIndex = indexOfFirstVowel(wordToCheck);
+				if (foundIndex > 0) {					
+					String prefix = wordToCheck.substring(0, foundIndex); //To remove vowel at the beginning, locate first letter.
+					String posix = wordToCheck.substring(foundIndex, wordToCheck.length());
+					posix += prefix; // To print the word without the first
+				    System.out.println(posix + "ay");
+				} else {
+					System.out.println("Word doesn't have vowels! OMG! Are you sure this is English?...");
+				}
+			}
+
+//			Asking the user if she/he wishes to continue
 			System.out.println("Would you like to translate another word? (y/n)");
 			userInput = scanner.next();
 		} while (userInput.equalsIgnoreCase("y"));
-
-	}
-	
-	
-	String vowels = "aeiouAEIOU";
-	if (vowels.indexOf(
-			Character.toLowerCase(
-					testString.charAt(0))) != -1) {
 		
+		scanner.close();
+		System.out.println("Program ended");
 	}
-
-	private static String hasVowel(wordToCheck) {
-	    	String vowels = "aeiouAEIOU";
-	    	if (wordToCheck.contains("a")
-	    			{
-	    	return vowels;
-	    }
-	    	  String s1 = "My name is GFG";
-	    	  
-	          // prints true
-	          System.out.println(s1.contains("GFG"));
-	   return
-	    }
-	    
+		
+	public static boolean isFirstLetterAVowel(String wordToCheck) {	    	
+		String vowels = "aeiouAEIOU";
+		char firstLetter = wordToCheck.charAt(0);
+		boolean result = vowels.contains(firstLetter + "");	   
+		return result;
+	}
+	
+	public static int indexOfFirstVowel(String wordToCheck) {
+		int foundIndex = -1;
+		for (int i = 0; i < wordToCheck.length(); i++) {
+			String vowels = "aeiouAEIOU";
+			char letter = wordToCheck.charAt(i);
+			boolean result = vowels.contains(letter + "");
+			if (result == true) {
+				foundIndex = i;
+				break;
+			}
+		}
+		return foundIndex;
+	}
 }
